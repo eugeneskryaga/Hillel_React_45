129c9df1aa1e4f88a5e60c234068c0eb73cd35aa
Eugene <zhenyaskryaga@gmail.com>
2026-04-03 21:10:09 +0300

    test

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -22,17 +22,19 @@ export const Reviews = () => {
     <>
       <h3>Reviews</h3>
       <ul>
-        {reviews?.map((review, index) => (
-          <li key={index}>
-            <p>
-              <strong>{review.reviewerName}</strong>
-            </p>
-            <p>Email: {review.reviewerEmail}</p>
-            <p>Date: {new Date(review.date).toLocaleDateString()}</p>
-            <p>Rating: {review.rating}</p>
-            <p>Review: {review.comment}</p>
-          </li>
-        ))}
+        {reviews &&
+          reviews.length > 0 &&
+          reviews.map((review, index) => (
+            <li key={index}>
+              <p>
+                <strong>{review.reviewerName}</strong>
+              </p>
+              <p>Email: {review.reviewerEmail}</p>
+              <p>Date: {new Date(review.date).toLocaleDateString()}</p>
+              <p>Rating: {review.rating}</p>
+              <p>Review: {review.comment}</p>
+            </li>
+          ))}
       </ul>
       {isLoading && <p>Loading...</p>}
       {isError && <p>It`s an error.</p>}
